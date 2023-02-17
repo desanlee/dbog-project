b2eebfcc39f57aabedcc813fd6f4bff92838cbb0
desanlee <desanlee@gmail.com>
2023-02-17 17:00:30 +0800

    update admin site

--- games/admin.py
+++ games/admin.py
@@ -7,42 +7,54 @@ import xlrd
 from .models import Series, Game, GameCard, CardConnection
 from tools.langconv import *
 
-class ConnectionInline(admin.StackedInline):
+class ConnectionInline(admin.TabularInline):
     model = CardConnection
+    fields = (('d_game','connection_type'),('s_para_1','s_para_2'),('d_para_1','d_para_2'))
     fk_name = 's_game'
-    extra = 1
+    extra = 0
+
+class GamesInline(admin.StackedInline):
+    model = Game
+    fields = ('discription',)
+    fk_name = 'series'
+    readonly_fields=('discription',)
+    extra = 0
+    can_delete = False
+    def has_add_permission(request, obj, t):
+        return False
+
+class GameCardAdmin(admin.ModelAdmin):
+    list_display = ('game', 'field_1', 'field_2', 'field_3', 'field_4', 'field_5', 'field_6')
+    actions_on_top = False
+    list_filter = ['game']
+
+    def has_add_permission(request, obj):
+        return False
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 class SeriesAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {
-            'fields': ['name','discription']
-            }),
-        (None, {
-            'fields': ['author']
-            }),
-    ]
+    fields = (('name','author'),('discription'))
+    inlines = [GamesInline]
+
     list_display = ('name', 'discription', 'author')
-    list_filter = ['name']
-    search_fields = ['name']
+    actions_on_top = False
 
 class GameAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {
-            'fields': ['series','name','xfile']
-            }),
-        (None, {
-            'fields': ['discription','restriction','note']
-            }),
-        (None, {
-            'fields': ['field_name_1','field_name_2','field_name_3','field_name_4','field_name_5','field_name_6']
-            }),
-    ]
+    fields = (('name','series','xfile'),
+        ('discription','restriction','note'),
+        ('field_name_1','field_name_2','field_name_3'),
+        ('field_name_4','field_name_5','field_name_6'))
     inlines = [ConnectionInline]
 
-    list_display = ('name', 'series', 'discription',
+    list_display = ('name', 'series',
         'field_name_1','field_name_2','field_name_3','field_name_4','field_name_5','field_name_6',
     )
-    search_fields = ['name']
+
+    list_filter = ['series']
+    actions_on_top = False
 
     def save_model(self, request, obj, form, change):
         # create new gamecard model and import dataset
@@ -75,6 +87,6 @@ class GameAdmin(admin.ModelAdmin):
 
             obj.xfile.delete()
 
-
 admin.site.register(Series,SeriesAdmin)
 admin.site.register(Game,GameAdmin)
+admin.site.register(GameCard,GameCardAdmin)
--- games/models.py
+++ games/models.py
@@ -3,21 +3,24 @@ from django.utils.translation import gettext_lazy as _
 
 class Series(models.Model):
     # this is what deboga is all about
-    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='丛书名称')
+    name = models.CharField(max_length=100, null=False, blank=False, verbose_name='游戏台名称')
     discription = models.CharField(max_length=500, null=False, blank=False, verbose_name='详细介绍')
-    cfile = models.ImageField(upload_to="covers", verbose_name='丛书封面')
+    cfile = models.ImageField(upload_to="covers", verbose_name='游戏台封面')
     author = models.CharField(max_length=100, null=True, blank=True, verbose_name='作者')
     status = models.CharField(max_length=20, null=True, blank=True, verbose_name='状态')
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = '游戏台'
+        verbose_name_plural = '游戏台'
 
 class Game(models.Model):
     # each game belongs to a category
-    series = models.ForeignKey(Series, null=True,blank=True, related_name='game_include',verbose_name='所属丛书名称', on_delete = models.CASCADE)
+    series = models.ForeignKey(Series, null=True,blank=True, related_name='game_include',verbose_name='所属游戏台', on_delete = models.CASCADE)
 
     # the basic info for a game
-    name = models.CharField(max_length=100, null=True, verbose_name='游戏名称')
-    xfile = models.FileField(null=True, blank=True, verbose_name='游戏卡数据文件')
+    name = models.CharField(max_length=100, null=True, verbose_name='游戏包名称')
+    xfile = models.FileField(null=True, blank=True, verbose_name='游戏包中数据卡的文件')
     # to make an expression for a game
     # users will know a game by watching these information
     discription = models.CharField(max_length=500, null=True, blank=True, verbose_name='游戏描述')
@@ -26,32 +29,38 @@ class Game(models.Model):
 
     # to indcate the meaning of fields in each game card
     # the gamecard model will be created based on following information
-    field_name_1 = models.CharField(max_length=100, null=True, blank=True)
-    field_name_2 = models.CharField(max_length=100, null=True, blank=True)
-    field_name_3 = models.CharField(max_length=100, null=True, blank=True)
-    field_name_4 = models.CharField(max_length=100, null=True, blank=True)
-    field_name_5 = models.CharField(max_length=100, null=True, blank=True)
-    field_name_6 = models.CharField(max_length=100, null=True, blank=True)
+    field_name_1 = models.CharField(max_length=100, null=True, blank=True, verbose_name='主要信息1')
+    field_name_2 = models.CharField(max_length=100, null=True, blank=True, verbose_name='主要信息2')
+    field_name_3 = models.CharField(max_length=100, null=True, blank=True, verbose_name='主要信息3')
+    field_name_4 = models.CharField(max_length=100, null=True, blank=True, verbose_name='连接信息1')
+    field_name_5 = models.CharField(max_length=100, null=True, blank=True, verbose_name='连接信息2')
+    field_name_6 = models.CharField(max_length=100, null=True, blank=True, verbose_name='连接信息3')
 
     card_count = models.IntegerField(null=True, blank=True, verbose_name='游戏卡数量')
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = '卡片包'
+        verbose_name_plural = '卡片包'
 
 class GameCard(models.Model):
     # each gamecard belongs to a game
-    game = models.ForeignKey(Game, null=True, blank=True, related_name='game_card', verbose_name='游戏', on_delete = models.CASCADE)
+    game = models.ForeignKey(Game, null=True, blank=True, related_name='game_card', verbose_name='卡片包', on_delete = models.CASCADE)
 
     # the basic info for a game
-    field_1 = models.CharField(max_length=500, null=True, blank=True)
-    field_2 = models.CharField(max_length=500, null=True, blank=True)
-    field_3 = models.CharField(max_length=500, null=True, blank=True)
-    field_4 = models.CharField(max_length=500, null=True, blank=True)
-    field_5 = models.CharField(max_length=500, null=True, blank=True)
-    field_6 = models.CharField(max_length=500, null=True, blank=True)
+    field_1 = models.CharField(max_length=500, null=True, blank=True, verbose_name='主要信息1')
+    field_2 = models.CharField(max_length=500, null=True, blank=True, verbose_name='主要信息2')
+    field_3 = models.CharField(max_length=500, null=True, blank=True, verbose_name='主要信息3')
+    field_4 = models.CharField(max_length=500, null=True, blank=True, verbose_name='连接信息1')
+    field_5 = models.CharField(max_length=500, null=True, blank=True, verbose_name='连接信息2')
+    field_6 = models.CharField(max_length=500, null=True, blank=True, verbose_name='连接信息3')
 
     def __str__(self):
         return self.field_1
+    class Meta:
+        verbose_name = '数据卡'
+        verbose_name_plural = '数据卡'
 
 class CardConnection(models.Model):
     # 当前计划支持多种关联模式，分别是：
@@ -60,21 +69,21 @@ class CardConnection(models.Model):
     # 空间相似关联，
     class ConnectionMode(models.TextChoices):
         # 实体相似
-        FIELDIS = 'FIS', _('Field is： 和目标是同样的字符串')
-        FIELDIN = 'FIN', _('Field in： 自己被包含在目标字符串中')
+        FIELDIS = 'FIS', _('Field is： 自己和目标字符串相同')
+        FIELDIN = 'FIN', _('Field in： 自己包含于目标字符串中')
         # 时间段重叠
-        TIMEHAS = 'THS', _('Time has： 自己的时间段涵盖了目标时间')
-        TIMEIN = 'TIN', _('Time in： 自己的时间被包括在目标时间段中')
+        TIMEHAS = 'THS', _('Time has： 自己的时间段包含目标时间')
+        TIMEIN = 'TIN', _('Time in： 自己的时间包含在目标时间段中')
         TIMECROSS = 'TCR', _('Time cross： 两个时间段有交集')
         # 空间重叠 TBD
 
     name = models.CharField(max_length=100, null=True, verbose_name='关联名称')
     discription = models.CharField(max_length=500, null=True, blank=True, verbose_name='关联描述')
 
-    s_game = models.ForeignKey(Game, null=False, blank=False, related_name='connection_from', verbose_name='来源游戏', on_delete = models.CASCADE)
-    s_para_1 = models.IntegerField(null=True, blank=True, verbose_name='源参1')
-    s_para_2 = models.IntegerField(null=True, blank=True, verbose_name='源参2')
-    s_para_3 = models.IntegerField(null=True, blank=True, verbose_name='源参3')
+    s_game = models.ForeignKey(Game, null=False, blank=False, related_name='connection_from', verbose_name='关联来源', on_delete = models.CASCADE)
+    s_para_1 = models.IntegerField(null=True, blank=True, verbose_name='来源参数1')
+    s_para_2 = models.IntegerField(null=True, blank=True, verbose_name='来源参数2')
+    s_para_3 = models.IntegerField(null=True, blank=True, verbose_name='来源参数3')
 
     connection_type = models.CharField(
         max_length=100,
@@ -83,10 +92,13 @@ class CardConnection(models.Model):
         verbose_name='关联类型'
     )
 
-    d_game = models.ForeignKey(Game, null=False, blank=False, related_name='connection_to', verbose_name='目标游戏', on_delete = models.CASCADE)
-    d_para_1 = models.IntegerField(null=True, blank=True, verbose_name='标参1')
-    d_para_2 = models.IntegerField(null=True, blank=True, verbose_name='标参2')
-    d_para_3 = models.IntegerField(null=True, blank=True, verbose_name='标参3')
+    d_game = models.ForeignKey(Game, null=False, blank=False, related_name='connection_to', verbose_name='关联目标', on_delete = models.CASCADE)
+    d_para_1 = models.IntegerField(null=True, blank=True, verbose_name='目标参数1')
+    d_para_2 = models.IntegerField(null=True, blank=True, verbose_name='目标参数2')
+    d_para_3 = models.IntegerField(null=True, blank=True, verbose_name='目标参数3')
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = '相关游戏卡'
+        verbose_name_plural = '所有相关游戏卡'
